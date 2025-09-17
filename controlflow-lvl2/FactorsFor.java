9a433a316f261168ad779bd925d9e17a71e7b7b9
amanchaudharyyyy <amanchaudhary0044@gmail.com>
2025-09-17 11:11:30 +0530

    Add files via upload

---
+++ controlflow-lvl2/FactorsFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorsFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            System.out.println("Factors of " + n + ":");
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Not a positive number.");
+        }
+    }
+}
